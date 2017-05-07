a9c54079c16609fa86204296d8ae7c47b354feae
Bhavika Tekwani <bhavicka.7992@gmail.com>
2017-05-07 04:37:18 -0400

    Feature engg refactor complete, LSTM - using Kaggle model

--- src/lstm.py
+++ src/lstm.py
@@ -3,15 +3,15 @@ import numpy as np
 from gensim.models import word2vec
 from tqdm import tqdm, tqdm_pandas
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Dropout
+from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM, GRU
 from keras.layers.normalization import BatchNormalization
 from keras.utils import np_utils
-# from keras.engine.topology import Merge
+from keras.models import Model
 from keras.layers import TimeDistributed, Lambda
 from keras.layers import Convolution1D, GlobalMaxPooling1D
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras import backend as K
 from keras.layers.advanced_activations import PReLU
 from keras.preprocessing import sequence, text
@@ -24,6 +24,7 @@ from utilities import utilities as u
 import re
 from string import punctuation
 from tqdm import tqdm, tqdm_pandas
+from keras.layers.merge import concatenate
 
 tqdm.pandas()
 
@@ -46,17 +47,17 @@ norm_model.init_sims(replace=True)
 STAMP = 'lstm_%d_%d_%.2f_%.2f'%(num_lstm, num_dense, rate_drop_lstm, \
         rate_drop_dense)
 
-train = joblib.load(u.TRAIN_PKL)
-test = joblib.load(u.TEST_PKL)
+train = pd.read_csv(u.TRAIN_PKL, sep=';')
+test = pd.read_csv(u.TEST_PKL, sep=';')
 
-q1 = train['question1'].tolist()
-q2 = train['question2'].tolist()
+q1 = train[0].tolist()
+q2 = train[1].tolist()
 
 print (q1)
 print (q2)
 
-q1_test = test['question1'].tolist()
-q2_test = test['question2'].tolist()
+q1_test = test[0].tolist()
+q2_test = test[1].tolist()
 
 q1_test = q1_test.tolist()
 q2_test = q2_test.tolist()
@@ -79,13 +80,6 @@ print('Found %s unique tokens' % len(word_index))
 data_1 = pad_sequences(sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
 data_2 = pad_sequences(sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
 labels = np.array(labels)
-
-# train
-# w2v_q1 = joblib.load('../data/q1_w2v.pkl')
-# w2v_q2 = joblib.load('../data/q2_w2v.pkl')
-#
-# print (w2v_q1.shape)
-# print (w2v_q2.shape)
 
 print ("Elapsed time till loading word vectors", time()-start)
 
@@ -120,9 +114,81 @@ for word, i in word_index.items():
         embedding_matrix[i] = wv_model.word_vec(word)
 print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
 
-joblib.dump('embedding_matrix_lstm.pkl', embedding_matrix)
+np.save('../data/embedding_matrix_lstm.npz', embedding_matrix)
 
 print ("Elapsed time till creating embedding matrix", time()-start_wv)
 
 print ("Elapsed time", time()-start)
 
+start_model = time()
+
+
+embedding_layer = Embedding(nb_words,
+        EMBEDDING_DIM,
+        weights=[embedding_matrix],
+        input_length=MAX_SEQUENCE_LENGTH,
+        trainable=False)
+lstm_layer = LSTM(num_lstm, dropout=rate_drop_lstm, recurrent_dropout=rate_drop_lstm)
+
+sequence_1_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedded_sequences_1 = embedding_layer(sequence_1_input)
+x1 = lstm_layer(embedded_sequences_1)
+
+sequence_2_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedded_sequences_2 = embedding_layer(sequence_2_input)
+y1 = lstm_layer(embedded_sequences_2)
+
+merged = concatenate([x1, y1])
+merged = Dropout(rate_drop_dense)(merged)
+merged = BatchNormalization()(merged)
+
+merged = Dense(num_dense, activation=act)(merged)
+merged = Dropout(rate_drop_dense)(merged)
+merged = BatchNormalization()(merged)
+
+preds = Dense(1, activation='sigmoid')(merged)
+
+########################################
+## add class weight
+########################################
+if re_weight:
+    class_weight = {0: 1.309028344, 1: 0.472001959}
+else:
+    class_weight = None
+
+########################################
+## train the model
+########################################
+model = Model(inputs=[sequence_1_input, sequence_2_input], \
+        outputs=preds)
+model.compile(loss='binary_crossentropy',
+        optimizer='nadam',
+        metrics=['acc'])
+#model.summary()
+print(STAMP)
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+bst_model_path = STAMP + '.h5'
+model_checkpoint = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=True)
+
+hist = model.fit([data_1_train, data_2_train], labels_train, \
+        validation_data=([data_1_val, data_2_val], labels_val, weight_val), \
+        epochs=200, batch_size=2048, shuffle=True, \
+        class_weight=class_weight, callbacks=[early_stopping, model_checkpoint])
+
+model.load_weights(bst_model_path)
+bst_val_score = min(hist.history['val_loss'])
+
+########################################
+## make the submission
+########################################
+print('Start making the submission before fine-tuning')
+
+preds = model.predict([test_data_1, test_data_2], batch_size=8192, verbose=1)
+preds += model.predict([test_data_2, test_data_1], batch_size=8192, verbose=1)
+preds /= 2
+
+submission = pd.DataFrame({'test_id':ids, 'is_duplicate':preds.ravel()})
+submission.to_csv('%.4f_'%(bst_val_score)+STAMP+'.csv', index=False)
+
+print ("Elapsed time", time()-start_model)
